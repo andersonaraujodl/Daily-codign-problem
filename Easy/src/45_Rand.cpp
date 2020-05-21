db0d34132e41c3367a2c77811911a634688cc282
andersonaraujodl <andersonaraujodl@gmail.com>
2020-05-21 20:41:51 -0300

    fix probability

--- Easy/src/45_Rand.cpp
+++ Easy/src/45_Rand.cpp
@@ -16,24 +16,39 @@ int rand5()
 	std::uniform_int_distribution<int> rand(1, 5);
 
 	int i = rand(rd);
-
-	std::cout << "Rand 5: " << i << std::endl;
 	return i;
 }
 
 int rand7()
 {
-	int i = ((rand5() + rand5()) * 5) % 7;
-	
-	i = (i == 0) ? 7 : i;
+	int i = 5 * rand5() + rand5() - 5; 
 
-	std::cout << "Rand 7: " << i << std::endl;
-	return i;
+	//To keep distribution
+	if (i < 22)
+		return i % 7 + 1;
+
+	return rand7();
 }
 
 #ifdef _45_Rand
 int main()
 {
-	std::cout << rand7() << std::endl;
+	const int total_runs = 100000;
+	int pesos[7] = { 0 };
+
+	for (int i = 0; i < total_runs; ++i)
+	{
+		int valor = rand7();
+		//std::cout << valor << ", ";
+
+		++pesos[valor - 1];
+	}
+
+	std::cout << "Pesos:" << std::endl;
+
+	for (int i = 0; i < 7; ++i)
+	{
+		std::cout << i + 1 << "=" << pesos[i] << ", ";
+	}
 }
 #endif
