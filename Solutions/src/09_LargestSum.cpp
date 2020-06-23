8809c8a507f827c4d6044aeed4c057b4972ccbac
Anderson Lima <andersonaraujodl@gmail.com>
2020-06-23 20:02:42 -0300

    fix bugs

--- Solutions/src/09_LargestSum.cpp
+++ Solutions/src/09_LargestSum.cpp
@@ -6,7 +6,7 @@ For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1,
 Follow-up: Can you do this in O(N) time and constant space?
 
 Date: 23/06/2020
-Time: 18:25 to 19h01
+Time: 18:25 to 19h01 - I found out my solution didn't work nicely, and found this simple solution that works perfectly
 */
 
 #include "const.h";
@@ -15,44 +15,22 @@ Time: 18:25 to 19h01
 
 int maxSum(int arr[], int &size)
 {
-	int sum = arr[0];
+	int incl = arr[0];
+	int excl = 0;
+	int excl_new;
+	int i;
 
-	for (int i = 2; i < size; ++i)
+	for (i = 1; i < size; i++)
 	{
-		if (sum <= 0 && arr[i - 1] > sum)
-		{
-			sum = arr[i - 1];
-		}
-		else if (i + 1 < size)
-		{
-			if (arr[i] > arr[i + 1])
-			{
-				if(sum > 0 && arr[i] > 0)
-					sum += arr[i];
-			}
-			else
-			{
-				if (sum < 0 && arr[i + 1] > sum)
-				{
-					sum = arr[i + 1];
-					++i;
-				}
-				if (sum > 0 && arr[i + 1] > 0)
-				{
-					sum += arr[i + 1];
-					++i;
-				}
-			}
-		}
-		else 
-		{
-			//if arr[i] < 0 it will decrese the number
-			if(arr[i] > 0)
-				sum += arr[i];
-		}
+		// current max excluding i
+		excl_new = (incl > excl) ? incl : excl;
+
+		// current max including i
+		incl = excl + arr[i];
+		excl = excl_new;
 	}
 
-	return sum;
+	return ((incl > excl) ? incl : excl);
 }
 
 #ifdef _09_LargestSum
@@ -68,10 +46,7 @@ int main()
 	int sizeC = ARRAY_SIZE(c);
 
 	std::cout << "Max Sum of A: " << maxSum(a, sizeA) << std::endl;
-
 	std::cout << "Max Sum of B: " << maxSum(b, sizeB) << std::endl;
-
 	std::cout << "Max Sum of C: " << maxSum(c, sizeC) << std::endl;
-
 }
 #endif;
